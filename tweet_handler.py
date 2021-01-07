acac60341eed705310411a15b4e40e24f6e54419
Cy Heffley <cheffley6@gatech.edu>
2021-01-07 10:57:56 -0500

    extract audio is working, uses ffmpeg python

--- tweet_handler.py
+++ tweet_handler.py
@@ -2,6 +2,7 @@ from config import misc, twitter_credentials
 from urllib.request import urlretrieve
 from pprint import pprint
 from twython import Twython
+import ffmpeg
 
 
 twitter = Twython(
@@ -17,12 +18,13 @@ def download(id, filename):
 
     urlretrieve(video_url, filename)
 
-# for now, only convert one video at a time
-# TO-DO: determine if speech-to-text requires wav or mp3
-# TO-DO:this must write the video's audio into misc.LATEST_AUDIO_NAME
-def convert_to_audio():
+# writes video's audio to LATEST_AUDIO_NAME
+def write_video_to_audio_file():
     video_path = misc.LATEST_VIDEO_NAME
-    pass
+    stream = ffmpeg.input(video_path)
+    audio = stream.audio
+    stream = ffmpeg.output(audio, misc.LATEST_AUDIO_NAME).overwrite_output()
+    ffmpeg.run(stream)   
 
 # TO-DO: this must post a reply to the original tweet with the predicted text from the audio
 def reply_to_tweet(text):
@@ -30,5 +32,5 @@ def reply_to_tweet(text):
 
 def process_one_video(tweet_id):
     download(tweet_id, misc.LATEST_VIDEO_NAME)
-    # audio_path = convert_to_audio(video_path)
+    write_video_to_audio_file()
 
